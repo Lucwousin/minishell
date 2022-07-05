f3f6546b5cd1e3d5c3a1173fe34a2e00653090c4
Lucas Sinke <lsinke@student.codam.nl>
2022-07-05 18:14:21 +0200

    lexer: Add documentation to token.h

--- include/token.h
+++ include/token.h
@@ -62,22 +62,79 @@ typedef struct s_token {
 	t_dynarr	sub;
 }	t_token;
 
+/**
+ * A token after expansion. All variables are expanded to their values, quotes
+ * are removed. Str is a freshly allocated string with the characters that make
+ * up this token.
+ */
 typedef struct s_expanded_token {
 	t_tokentype	type;
 	char		*str;
 }	t_exp_token;
 
+/**
+ * A simple struct used as an argument for expand_token (expander.c)
+ * 
+ * _cmd_ is the user input, _index_ is the current index in _expanded_
+ */
 typedef struct s_expander_argument {
 	t_exp_token	*expanded;
 	const char	*cmd;
 	size_t		index;
 }	t_exp_arg;
 
+/**
+ * Tokenize the user input. Splits everything up into words and operators.
+ * Words can have sub-tokens (quotes and variables). Quotes also can have subs
+ * (variables).
+ * 
+ * @param tokens[out] A dynamic array all the tokens will be added to.
+ * @param cmd[in] The user input
+ */
 void		tokenize(t_dynarr *tokens, const char *cmd);
+
+/**
+ * Match a token!
+ * 
+ * @param cmd[in] The user input 
+ * @param idx[in/out] The current index in the user input
+ * @param token[in/out] The current token we're creating. Starts off as EOF.
+ */
 void		match_token(const char *cmd, size_t *idx, t_token *token);
+
+/**
+ * Match an operator!
+ * 
+ * @param cmd[in] The user input
+ * @param idx[in/out] the current index in the user input
+ * @param token[in/out] The current token we're creating (set to OPERATOR)
+ */
 void		match_operator(const char *cmd, size_t *idx, t_token *token);
+
+/**
+ * Match a word!
+ * 
+ * @param cmd[in] The user input
+ * @param idx[in/out] the current index in the user input
+ * @param token[in/out] The current token we're creating (set to WORD)
+ * 
+ * This function recursively calls match_token, until the current token is
+ * delimited by the end of input, whitespace, or an operator
+ * If there's a quoted part in this word, it is added as a subtoken to the word.
+ * If there's a variable in this word, or in a quote, it is added as a subtoken
+ * to the token it is part of (so a quote, or the word)
+ */
 void		match_word(const char *cmd, size_t *idx, t_token *token);
 
+/**
+ * Expand all tokens!
+ * 
+ * @param tokens[in] All tokens returned from tokenize 
+ * @param cmd[in] The user input
+ * 
+ * @return An array of all expanded tokens, delimited by a expanded token with
+ * type END_OF_INPUT.
+ */
 t_exp_token	*expand(t_dynarr *tokens, const char *cmd);
 
 #endif
