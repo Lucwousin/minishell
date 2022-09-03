2edcbb3c383d1ac448de4c261598795e3fdcc20f
Lucas Sinke <lsinke@student.codam.nl>
2022-09-03 20:43:51 +0200

    Skip whitespace around quoted exit args

--- src/executor/builtins/builtin_exit.c
+++ src/executor/builtins/builtin_exit.c
@@ -19,26 +19,35 @@
 #define ERR_ARGS	"too many arguments"
 #define ERR_NUMS	": numeric argument required"
 
+static const char	*skip_whitespace(const char *arg)
+{
+	while (*arg == ' ' || *arg == '\t' || *arg == '\n')
+		++arg;
+	return (arg);
+}
+
 static bool	parse_status(const char *arg, uint8_t *status)
 {
-	int8_t	sign;
-	int64_t	parse;
+	int8_t		sign;
+	uint64_t	parse;
 
+	arg = skip_whitespace(arg);
 	sign = 1;
 	if (*arg == '-')
 		sign = -1;
 	if (*arg == '-' || *arg == '+')
 		++arg;
 	parse = 0;
-	while (*arg)
+	while (ft_isdigit(*arg))
 	{
-		if (!ft_isdigit(*arg))
-			return (false);
 		parse *= 10;
 		parse += (*arg++ - '0');
-		if (parse < 0)
+		if ((sign == 1 && parse > INT64_MAX) || (sign == -1 && parse > (uint64_t) INT64_MIN))
 			return (false);
 	}
+	arg = skip_whitespace(arg);
+	if (*arg != '\0')
+		return (false);
 	*status = sign * parse;
 	return (true);
 }
